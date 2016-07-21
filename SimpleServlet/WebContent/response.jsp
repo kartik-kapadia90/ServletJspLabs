2c8112aabfe62808369b3ebc733fe03f7e0285c0
kkapadia <kkapadia@visa.com>
2016-07-21 18:03:12 -0500

    Day 3 Servlet JSP Lab

---
+++ SimpleServlet/WebContent/response.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Response Page</title>
+</head>
+<body>
+Hello World from JSP
+
+
+<br />
+<p>The message (printed using expression language) is: </p>
+<p>${message}</p>
+</body>
+</html>
--- SimpleServlet/src/com/demo/HelloWorld.java
+++ SimpleServlet/src/com/demo/HelloWorld.java
@@ -1,8 +1,8 @@
 package com.demo;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorld extends HttpServlet{
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
-		resp.getWriter().write(LocalDateTime.now().toString());
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
+		//resp.getWriter().write(LocalDateTime.now().toString());
+		req.setAttribute("message", "Welcome, today is Wednesday");
+		req.getRequestDispatcher("response.jsp").forward(req, resp);
 	}
 
 }
